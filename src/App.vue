2b2d99b8965a3cac7b7fc6b8d0f235cc9e19b6be
Mathias Sam <mathiassam2@gmail.com>
2024-05-23 16:38:13 +0800

    added font awesome

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+  <successmodal/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import successmodal from './components/success-modal.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    successmodal
+  },
+  data() {
+    return {
+      title: 'Vue App'
+    }
+  },
+  methods: {
+    
   }
 }
 </script>
@@ -22,5 +29,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/success-modal.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.modal {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: auto;
+    height: auto;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    z-index: 100;
+}
+.backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 99;
+}
+h1 {
+    border: none;
+    padding: 0;
+}
+</style>
